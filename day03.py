64edc76f322be086a2168b9b2c570f117dfed1d0
Daniel Ong <danielong1@gmail.com>
2018-12-02 22:25:49 -0800

    Improve part 2

--- day03.py
+++ day03.py
@@ -11,7 +11,6 @@ for claim in inp:
     id, _, pos, size = claim.split(' ')
     row, col = (int(x) for x in pos[:-1].split(','))
     side1, side2 = [int(x) for x in size.split('x')]
-
     arr[row:row + side1, col:col + side2] += 1
 print((arr > 1).sum())
 
@@ -21,6 +20,6 @@ for claim in inp:
     row, col = (int(x) for x in pos[:-1].split(','))
     side1, side2 = [int(x) for x in size.split('x')]
 
-    if arr[row:row + side1, col:col + side2].sum() == side1 * side2:
+    if np.all(arr[row:row + side1, col:col + side2] == 1):
         print(id)
         break
