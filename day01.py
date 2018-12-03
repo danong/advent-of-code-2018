efe2025e2219dde1c2edb0a3362f26c52212be3d
Daniel Ong <danielong1@gmail.com>
2018-12-02 22:13:59 -0800

    Move input

--- day01.py
+++ day01.py
@@ -1,17 +1,15 @@
-inp = """+3
-+3
-+4
--2
--4"""
+with open('input/day01.txt', 'r') as f:
+    inp = [l.strip() for l in f.readlines()]
+
 # part 1
-val = ''.join(inp.split('\n'))
+val = ''.join(inp)
 print(eval(val))
 
 # part 2
 start = 0
 seen = {0}
 while True:
-    for num in inp.splitlines():
+    for num in inp:
         start += int(num)
         if start in seen:
             print(start)
--- day02.py
+++ day02.py
@@ -1,30 +1,23 @@
-inp = """ivyhczwokexltwhsfamqprbnuy
-ivjhcjdokexltwwsfamqpabnuy
-ivjhczdokebltwgsfydqprbnuy
-ivjhczdoknxltwgssamqsrbnuy
-ivjhczdokexltwgswadqprbruy
-ivjhcjdokexltwfsfamqpabnuy
-uvjhczrozexltwgsfamqprbnuy
-ivjhjzdxkexltwgsffpqprbnuy"""
-
 from collections import Counter
+
+with open('input/day02.txt', 'r') as f:
+    inp = [l.strip() for l in f.readlines()]
 
 # part 1
 twos = 0
 threes = 0
-for id in inp.splitlines():
-  count = Counter(id)
-  if 2 in count.values():
-    twos += 1
-  if 3 in count.values():
-    threes += 1
+for id in inp:
+    count = Counter(id)
+    if 2 in count.values():
+        twos += 1
+    if 3 in count.values():
+        threes += 1
 print(twos * threes)
 
 # part 2
-lines = inp.splitlines()
-for idx1 in range(len(lines)):
-    for idx2 in range(idx1, len(lines)):
-        id1, id2 = lines[idx1], lines[idx2]
+for idx1 in range(len(inp)):
+    for idx2 in range(idx1, len(inp)):
+        id1, id2 = inp[idx1], inp[idx2]
         dif = 0
         for idxc in range(len(id1)):
             if id1[idxc] != id2[idxc]:
