13a975809ce63edca4d304ef099b6a1060ebc140
Daniel Ong <danielong1@gmail.com>
2018-12-07 21:57:45 -0800

    clean day08.py

--- day08.py
+++ day08.py
@@ -5,35 +5,34 @@ with open('input/day08.txt', 'r') as f:
 
 def visit(start):
     meta_sum = 0
-    num_nodes = inp[start]
-    num_meta = inp[start + 1]
+    num_nodes, num_meta = inp[start: start + 2]
     next_start = start + 2
     for child_node in range(num_nodes):
         t_sum, next_start = visit(next_start)
         meta_sum += t_sum
-    meta_sum += sum(inp[next_start:next_start+num_meta])
-    return meta_sum, next_start+num_meta
+    meta_sum += sum(inp[next_start:next_start + num_meta])
+    return meta_sum, next_start + num_meta
+
 
 def visit2(start):
-    meta_sum = 0
-    num_nodes = inp[start]
-    num_meta = inp[start + 1]
+    node_sum = 0
+    num_nodes, num_meta = inp[start: start + 2]
     next_start = start + 2
     if num_nodes:
-        meta_sums = []
+        node_vals = []
         for child_node in range(num_nodes):
             t_sum, next_start = visit2(next_start)
-            meta_sums.append(t_sum)
-        for idx in inp[next_start:next_start+num_meta]:
-            idx = idx - 1
-            if idx < len(meta_sums):
-                meta_sum += meta_sums[idx]
+            node_vals.append(t_sum)
+        for idx in inp[next_start:next_start + num_meta]:
+            if idx - 1 < len(node_vals):
+                node_sum += node_vals[idx - 1]
     else:
-        meta_sum += sum(inp[next_start:next_start+num_meta])
-    return meta_sum, next_start+num_meta
+        node_sum += sum(inp[next_start:next_start + num_meta])
+    return node_sum, next_start + num_meta
 
 
 def main():
+    print(visit(0))
     print(visit2(0))
 
 
