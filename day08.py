33cfd67b73a69ae1af74da8af820ef52d188cc69
Daniel Ong <danielong1@gmail.com>
2018-12-07 21:46:26 -0800

    add day08.py

--- day08.py
+++ day08.py
@@ -1,2 +1,40 @@
 with open('input/day08.txt', 'r') as f:
-    inp = [l.strip() for l in f.readlines()]
+    inp = f.read()
+    inp = [int(x) for x in inp.split(' ')]
+
+
+def visit(start):
+    meta_sum = 0
+    num_nodes = inp[start]
+    num_meta = inp[start + 1]
+    next_start = start + 2
+    for child_node in range(num_nodes):
+        t_sum, next_start = visit(next_start)
+        meta_sum += t_sum
+    meta_sum += sum(inp[next_start:next_start+num_meta])
+    return meta_sum, next_start+num_meta
+
+def visit2(start):
+    meta_sum = 0
+    num_nodes = inp[start]
+    num_meta = inp[start + 1]
+    next_start = start + 2
+    if num_nodes:
+        meta_sums = []
+        for child_node in range(num_nodes):
+            t_sum, next_start = visit2(next_start)
+            meta_sums.append(t_sum)
+        for idx in inp[next_start:next_start+num_meta]:
+            idx = idx - 1
+            if idx < len(meta_sums):
+                meta_sum += meta_sums[idx]
+    else:
+        meta_sum += sum(inp[next_start:next_start+num_meta])
+    return meta_sum, next_start+num_meta
+
+
+def main():
+    print(visit2(0))
+
+
+main()
